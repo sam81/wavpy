b0dd58b3d4ca391d16e687db4d2102a11eaa5a19
Samuele Carcagno <sam.carcagno@gmail.com>
2014-03-13 11:05:49 +0000

    Initial import

---
+++ prep-release/make_deb.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, platform, time
+
+#get current version number from setup.py
+f = open('../setup.py', 'r')
+ln = f.readlines()
+f.close()
+for i in range(len(ln)):
+    if ln[i].strip().split('=')[0].strip() == "version":
+           ver = ln[i].strip().split('=')[1].strip()
+           ver = ver[1:len(ver)-2]
+tarball_path = "../dist/wavpy-" + ver + ".tar.gz"
+buildpath = "../../pkg_build/" + platform.linux_distribution()[0] + '_' + platform.linux_distribution()[1].replace('/', '_') + '_' + platform.uname()[4]
+
+if os.path.exists(buildpath) == False:
+    os.makedirs(buildpath)
+
+#copy tarball to the build directory
+cmd = "cp " + tarball_path + " " + buildpath
+os.system(cmd)
+
+#update debian changelog
+f = open('debian/changelog', 'r')
+ln = f.readlines()
+f.close()
+
+l0 = ['wavpy (' + ver + ') unstable; urgency=low',
+      '\n\n',
+      '  * New upstream release',
+      '\n\n',
+      ' -- Samuele Carcagno <sam.carcagno@gmail.com>  ' + time.strftime("%a, %d %b %Y %H:%M:%S +0100", time.gmtime()),
+      '\n\n']
+l0.extend(ln)
+f = open('debian/changelog', 'w')
+ln = f.writelines(l0)
+f.close()
+
+#move to the build directory
+os.chdir(buildpath)
+
+#unpack the tarball
+cmd2 = "tar -xvf " + "wavpy-" + ver + ".tar.gz"
+os.system(cmd2)
+
+#copy debian files in the package build directory
+cmd3 = "cp -R " + "../../wavpy/prep-release/debian/ ./wavpy-" + ver
+os.system(cmd3)
+
+#move into package build directory
+os.chdir("wavpy-" + ver)
+#build the package
+os.system("dpkg-buildpackage -F")
---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+
+setup(name="wavpy",    
+    version="0.0.0",
+      py_modules=["wavpy"],
+      author="Samuele Carcagno",
+      author_email="sam.carcagno@gmail.com;",
+      description="wavpy is a python module to read and write WAV files",
+      long_description=\
+      """
+      Module for reading and writing WAV files. It is a simple but convenient wrapper to the scipy.io.wavfile module.
+      """,
+      license="GPL v3",
+      url="",
+      requires=['numpy (>=1.6.1)'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3.2',
+          'Topic :: Scientific/Engineering'
+          ]
+      )
